e4d1c2d4ecc192e780aaa33768af3ad71a900d5c
ruru112233 <esxwaz1234@gmail.com>
2022-01-08 12:39:28 +0900

    Enemy1Controllerの修正

--- Assets/Ruru/Scripts/Enemy/Enemy1Controller.cs
+++ Assets/Ruru/Scripts/Enemy/Enemy1Controller.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class Enemy1Controller : MonoBehaviour
 {
+    bool isGate = false;
+
+    float countTime = 0;
+    [SerializeField] float distanceTime = 2.0f;
+
+    [SerializeField] float xPos, yPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGate = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGate)
+        {
+            countTime += Time.deltaTime;
+        }
 
+        if (countTime > distanceTime)
+        {
+            transform.position += new Vector3(xPos * Time.deltaTime, yPos * Time.deltaTime, 0);
+        }
     }
 
-    private void OnBecameVisible()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("ƒJƒƒ‰‚É“ü‚Á‚½");
+        if (collision.gameObject.CompareTag("Gate"))
+        {
+            isGate = true;
+        }
     }
 }
